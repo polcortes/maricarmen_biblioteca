b03545e946ddef7f98eec5800c3b06f53240d226
Xavi Martinez <xaavii0711@gmail.com>
2024-04-24 08:43:10 +0200

    .

--- maricarmen_biblioteca/settings.py
+++ maricarmen_biblioteca/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'maricarmen_biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-	'USER': os.getenv('DB_USER'),
-	'PASSWORD': os.getenv('DB_PASSWORD'),
-	'HOST': os.getenv('DB_HOST'),
-	'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
