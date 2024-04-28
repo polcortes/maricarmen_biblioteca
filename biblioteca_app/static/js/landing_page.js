78811f050071aa1b5b161ca566595b1e8e78a700
R0sac <oscarhortelano2001@gmail.com>
2024-04-28 18:35:06 +0200

    6g8i68

--- biblioteca_app/static/js/landing_page.js
+++ biblioteca_app/static/js/landing_page.js
@@ -1,28 +1,32 @@
+//src="https://code.jquery.com/jquery-3.6.0.min.js"
+//src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui/1.12.1/jquery-ui.min.js"
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Función para manejar el envío del formulario
-    /*
-    document.querySelector("form").addEventListener("submit", function(event) {
-        event.preventDefault(); // Evitar el envío del formulario por defecto
+    //Función para manejar el envío del formulario
+    // document.querySelector("form").addEventListener("submit", function(event) {
+    //     event.preventDefault(); // Evitar el envío del formulario por defecto
         
-        // Obtener los valores de usuario y contraseña
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+    //     // Obtener los valores de usuario y contraseña
+    //     var username = document.getElementById("username").value;
+    //     var password = document.getElementById("password").value;
 
-        // Determinar el tipo de usuario
-        var isAdmin = isUserAdmin(username);
+    //     // Determinar el tipo de usuario
+    //     var isAdmin = isUserAdmin(username);
 
-        // Redirigir según el tipo de usuario
-        if (isAdmin) {
-            window.location.href = "http://127.0.0.1:8000/dashboard/admin/";
-        } else {
-          console.error("Error en la respuesta de la API:", data.error);
-        }
-    });
-    */
+    //     // Redirigir según el tipo de usuario
+    //     if (isAdmin) {
+    //         window.location.href = "http://127.0.0.1:8000/dashboard/admin/";
+    //     } else {
+    //         window.location.href = "http://127.0.0.1:8000/dashboard/general/";
+    //     }
+    // });
 
-    // Función para determinar si el usuario es administrador o no
+    //Función para determinar si el usuario es administrador o no
     function isUserAdmin(username) {
         // Supongamos que el usuario es admin si el nombre de usuario contiene la palabra "admin"
         return username.toLowerCase().includes("admin");
     }
+    
+    
+
 });
