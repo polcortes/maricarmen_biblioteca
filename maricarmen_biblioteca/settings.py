1f0b3d62dadc70ccc549a83467740454455258a3
Xavi Martinez <xaavii0711@gmail.com>
2024-05-02 16:08:54 +0200

    .

--- maricarmen_biblioteca/settings.py
+++ maricarmen_biblioteca/settings.py
@@ -92,17 +92,23 @@ WSGI_APPLICATION = 'maricarmen_biblioteca.wsgi.application'
 #     }
 # }
 
-
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-	'USER': os.getenv('DB_USER'),
-	'PASSWORD': os.getenv('DB_PASSWORD'),
-	'HOST': os.getenv('DB_HOST'),
-	'PORT': '',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-} 
+}
+
+# DATABASES = {
+#    'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('DB_NAME'),
+# 	'USER': os.getenv('DB_USER'),
+# 	'PASSWORD': os.getenv('DB_PASSWORD'),
+# 	'HOST': os.getenv('DB_HOST'),
+# 	'PORT': '',
+#     }
+# } 
 
 
 # Password validation
