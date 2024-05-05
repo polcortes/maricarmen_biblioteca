930060d72da85e1f3d7ea1b3f2c5236e628fe9fe
polcortes <pcortesgarcia.cf@iesesteveterradas.cat>
2024-05-05 02:10:52 +0200

    Responsive design, cdu + estat exemplars a la cerca y filtres menys any d'edició.

--- maricarmen_biblioteca/settings.py
+++ maricarmen_biblioteca/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 #? Para generar el requirements.txt: pip freeze > requirements.txt
 #? Para instalar las dependencias: pip install -r requirements.txt
 import os
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 import os
 
@@ -85,24 +85,24 @@ WSGI_APPLICATION = 'maricarmen_biblioteca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv('DB_NAME'),
-# 	'USER': os.getenv('DB_USER'),
-# 	'PASSWORD': os.getenv('DB_PASSWORD'),
-# 	'HOST': os.getenv('DB_HOST'),
-# 	'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+	'USER': os.getenv('DB_USER'),
+	'PASSWORD': os.getenv('DB_PASSWORD'),
+	'HOST': os.getenv('DB_HOST'),
+	'PORT': os.getenv('DB_PORT'),
+    }
+} 
 
 
 # Password validation
