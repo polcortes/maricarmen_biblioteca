8564cc06130eb7af18e57012b59309430f334f90
R0sac <oscarhortelano2001@gmail.com>
2024-05-06 20:54:05 +0200

    dtfyghuij

--- maricarmen_biblioteca/settings.py
+++ maricarmen_biblioteca/settings.py
@@ -112,30 +112,6 @@ DATABASES = {
 }
 
 
-DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-	'USER': os.getenv('DB_USER'),
-	'PASSWORD': os.getenv('DB_PASSWORD'),
-	'HOST': os.getenv('DB_HOST'),
-	'PORT': os.getenv('DB_PORT'),
-    }
-} 
-
-
-DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': os.getenv('DB_NAME'),
-	'USER': os.getenv('DB_USER'),
-	'PASSWORD': os.getenv('DB_PASSWORD'),
-	'HOST': os.getenv('DB_HOST'),
-	'PORT': os.getenv('DB_PORT'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
